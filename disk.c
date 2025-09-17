d68de5afe5f28be76f770770b92c40dfca86918b
Gabriel Bauer <gabeb1277@gmail.com>
2025-09-17 14:07:23 -0400

    began implementing disk.c which will be part of the starter code - taking the place of pages.c in the project 2 starter code

---
+++ disk.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <assert.h>
+
+#include "disk.h"
+
+// Disk operations
+DiskInterface* disk_open(const char* filename)
+{
+	DiskInterface *disk = (DiskInterface*)malloc(sizeof(DiskInterface));
+	struct stat fs_info;
+	
+	if (stat(filename, &fs_info) != 0) {
+		fprintf(stderr, "Failed to stat filesystem!!");
+	}
+	
+	disk->disk_file = open(filename, O_RDWR, 0644);
+	assert(disk->disk_file != -1);
+	
+	disk->disk_base = mmap(0, fs_info.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, disk->disk_file, 0);
+}
+
+void disk_close(DiskInterface* disk);
+int disk_read_block(DiskInterface* disk, uint64_t block_num, void* buffer);
+int disk_write_block(DiskInterface* disk, uint64_t block_num, const void* buffer);
+int disk_format(DiskInterface* disk, const char* volume_name);
