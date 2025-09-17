ba2c7154fc162b7d480099d3bbde74581c96658a
Gabriel Bauer <gabeb1277@gmail.com>
2025-09-17 14:10:27 -0400

    disk_close()

--- disk.c
+++ disk.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <assert.h>
 
 #include "disk.h"
@@ -19,9 +20,17 @@ DiskInterface* disk_open(const char* filename)
 	assert(disk->disk_file != -1);
 	
 	disk->disk_base = mmap(0, fs_info.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, disk->disk_file, 0);
+	
+	return disk;
 }
 
-void disk_close(DiskInterface* disk);
+void disk_close(DiskInterface* disk)
+{
+	munmap(disk->disk_base);
+	close(disk->disk_file);
+	free(disk);
+}
+
 int disk_read_block(DiskInterface* disk, uint64_t block_num, void* buffer);
 int disk_write_block(DiskInterface* disk, uint64_t block_num, const void* buffer);
 int disk_format(DiskInterface* disk, const char* volume_name);
